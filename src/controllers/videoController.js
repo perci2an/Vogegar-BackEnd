32cc8222fc5bd6b4f1dffa1354ae5f4405795eef
perci2an <122770473+perci2an@users.noreply.github.com>
2024-05-18 18:48:39 +0900

    비디오 파트 백앤드 분리

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,45 +1,44 @@
 import Video from "../models/video";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({}).sort({ createdAt: "desc" });
-  return res.render("home", { pageTitle: "Home", videos });
+  try {
+    const videos = await Video.find({}).sort({ createdAt: "desc" });
+    res.status(200).json({ videos });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const watch = async (req, res) => {
-  const { id } = req.params;
-  const video = await Video.findById(id);
-  if (!video) {
-    return res.status(404).render("404", { pageTitle: "Video not found." });
+  try {
+    const { id } = req.params;
+    const video = await Video.findById(id);
+    if (!video) {
+      res.status(404).json({ message: "비디오를 찾을 수 없습니다." });
+      return;
+    }
+    res.status(200).json({ video });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
-  return res.render("watch", { pageTitle: video.title, video });
-};
-
-export const getEdit = async (req, res) => {
-  const { id } = req.params;
-  const video = await Video.findById(id);
-  if (!video) {
-    return res.status(404).render("404", { pageTitle: "Video not found." });
-  }
-  return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
 
 export const postEdit = async (req, res) => {
   const { id } = req.params;
   const { title, description, hashtags } = req.body;
-  const video = await Video.exists({ _id: id });
-  if (!video) {
-    return res.status(404).render("404", { pageTitle: "Video not found." });
+  try {
+    const video = await Video.findByIdAndUpdate(id, {
+      title,
+      description,
+      hashtags: Video.formatHashtags(hashtags),
+    });
+    if (!video) {
+      return res.status(404).json({ message: "비디오를 찾을 수 없습니다. " });
+    }
+    return res.redirect(`/video/${id}`);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
   }
-  await Video.findByIdAndUpdate(id, {
-    title,
-    description,
-    hashtags: Video.formatHashtags(hashtags),
-  });
-  return res.redirect(`/video/${id}`);
-};
-
-export const getUpload = (req, res) => {
-  return res.render("upload", { pageTitle: "Upload Video" });
 };
 
 export const postUpload = async (req, res) => {
@@ -52,29 +51,23 @@ export const postUpload = async (req, res) => {
     });
     return res.redirect("/");
   } catch (error) {
-    console.log(error);
-    return res.status(400).render("upload", {
-      pageTitle: "Upload Video",
-      errorMessage: error._message,
-    });
+    return res.status(400).render({ message: error.message });
   }
-};
-
-export const deleteVideo = async (req, res) => {
-  const { id } = req.params;
-  await Video.findByIdAndDelete(id);
-  return res.redirect("/");
 };
 
 export const search = async (req, res) => {
   const { keyword } = req.query;
   let videos = [];
-  if (keyword) {
-    videos = await Video.find({
-      title: {
-        $regex: new RegExp(keyword, "i"),
-      },
-    });
+  try {
+    if (keyword) {
+      videos = await Video.find({
+        title: {
+          $regex: new RegExp(keyword, "i"),
+        },
+      });
+    }
+    return res.render("search", { videos });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
   }
-  return res.render("search", { pageTitle: "Search", videos });
 };
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,18 +1,10 @@
 import express from "express";
-import {
-  watch,
-  getEdit,
-  postEdit,
-  getUpload,
-  postUpload,
-  deleteVideo,
-} from "../controllers/videoController";
+import { watch, postEdit, postUpload } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
-videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
-videoRouter.route("/upload").get(getUpload).post(postUpload);
+videoRouter.post("/:id([0-9a-f]{24})/edit", postEdit);
+videoRouter.post("/upload", postUpload);
 
 export default videoRouter;
